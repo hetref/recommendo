3f5b6731e5f793d9058433efdf23af56a53309e7
Aryan shinde <shindearyan179@gmail.com>
2024-03-13 14:29:59 +0530

    RESET

--- app/page.js
+++ app/page.js
@@ -1,11 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-
 export default function Home() {
   return (
     <>
       <h1>Hello User</h1>
-      <UserButton />
     </>
   );
 }
