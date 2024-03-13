2d0d926294c506205967b1173bb71852f4ff66fd
Aryan shinde <shindearyan179@gmail.com>
2024-03-13 15:15:40 +0530

    Added Mongo Data

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { createUser } from "@/lib/actions/user";
+import { useState } from "react";
+
+export default function Header() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const saveData = async () => {
+    console.log("Name", name);
+    console.log("Email", email);
+
+    const newUser = await createUser({ name, email });
+
+    console.log(newUser);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Enter Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button onClick={saveData}>Save Data</button>
+    </>
+  );
+}
